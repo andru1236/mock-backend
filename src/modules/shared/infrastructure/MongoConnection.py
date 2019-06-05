8f8d38ba65d5eedb1a1c4d4c6439c0ec049c716f
Andres Gutierrez P <andres.gutierrez@jalasoft.com>
2019-06-04 20:57:58 -0400

    Implement mongo connection and create .env example

---
+++ src/modules/shared/infrastructure/MongoConnection.py
@@ -0,0 +1,16 @@
+import os
+
+from pymongo import MongoClient
+
+from modules.shared.domain import IPersistenceConnection
+
+
+class MongoConnection(IPersistenceConnection):
+    __db = None
+
+    @classmethod
+    def get_connection(cls):
+        if cls.__db is None:
+            mongo_link = MongoClient(str(os.environ.get('MONGO_CONNECTION')))
+            cls.__db = mongo_link[str(os.environ.get('DB_NAME_MONGO'))]
+        return cls.__db
