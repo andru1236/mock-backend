6689ed07b228c29f739683855896244ae74d062c
Andres Gutierrez P <andres.gutierrez@jalasoft.com>
2019-06-05 16:54:58 -0400

    Add unit test (add_route) to api instance

--- src/test/modules/api_instance/domain/ApiInstance_test.py
+++ src/test/modules/api_instance/domain/ApiInstance_test.py
@@ -1,6 +1,8 @@
 import pytest
 
 from modules.api_instance.domain import ApiInstance
+from modules.api_instance.domain import Response
+from modules.api_instance.domain import Route
 
 
 @pytest.mark.usefixtures('get_port', 'get_routes_crud')
@@ -11,3 +13,17 @@ def test_create_api_instance_success(get_port, get_routes_crud):
         assert api.routes[index].method == route.method
         assert api.routes[index].response.value == route.response.value
         assert api.routes[index].path == route.path
+
+
+@pytest.mark.usefixtures('get_port', 'get_routes_crud')
+def test_add_route_to_api(get_port, get_routes_crud):
+    api = ApiInstance(get_port, get_routes_crud)
+    count_routes = len(api.routes)
+
+    new_route = Route('apis', 'delete', Response('{"status": 200}'))
+    api.add_route(new_route)
+
+    assert count_routes != len(api.routes)
+    assert count_routes + 1 == len(api.routes)
+
+
