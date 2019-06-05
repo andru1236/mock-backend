6e70477e5e319b6cab21c62680b4da032a7b551c
Andres Gutierrez P <andres.gutierrez@jalasoft.com>
2019-06-04 20:00:07 -0400

    Implement register_api use case

--- src/modules/api_instance/application/register_api.py
+++ src/modules/api_instance/application/register_api.py
@@ -1,1 +1,21 @@
+from modules.api_instance.domain import ApiInstance
+from modules.api_instance.domain import IRepository
+from modules.api_instance.domain import Port
+from modules.shared.domain import ICommand
+from modules.shared.domain import IUseCase
+
+
 class RegisterApiCommand(ICommand):
+    def __init__(self, port: int) -> None:
+        self.port = port
+
+
+class RegisterApi(IUseCase):
+
+    def __init__(self, repository: IRepository) -> None:
+        self.repository = repository
+
+    def execute(self, command: RegisterApiCommand):
+        port = Port(command.port)
+        api = ApiInstance(port)
+        self.repository.save(api)
