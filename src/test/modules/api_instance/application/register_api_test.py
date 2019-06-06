0d0b519aae97b1e44f3612f23501133c6017db6d
Andres Gutierrez P <andres.gutierrez@jalasoft.com>
2019-06-06 12:01:05 -0400

    test_insert_many_apis

--- src/test/modules/api_instance/application/register_api_test.py
+++ src/test/modules/api_instance/application/register_api_test.py
@@ -18,3 +18,22 @@ def test_register_api_success():
     one_element_in_repository = 1
 
     assert len(repository.apis) == one_element_in_repository
+
+
+def test_register_many_apis_success():
+    repository = FakeRepository()
+    use_case = RegisterApi(repository)
+
+    not_exist_data = 0
+    assert len(repository.apis) == not_exist_data
+    common_port = [8000, 5000, 8080, 3000, 3001, 3002, 3003, 4000]
+
+    commands_will_be_execute = []
+    for port in common_port:
+        commands_will_be_execute.append(RegisterApiCommand(port))
+
+    inserted_apis = 0
+    for command in commands_will_be_execute:
+        use_case.execute(command)
+        inserted_apis += 1
+        assert len(repository.apis) == inserted_apis
