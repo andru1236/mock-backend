88fe2df98fecca3d46ab8cf40f670c02725583f0
Andres Gutierrez P <andres.gutierrez@jalasoft.com>
2019-06-06 14:23:19 -0400

    Implement search api use case

---
+++ src/modules/api_instance/application/search_api.py
@@ -0,0 +1,28 @@
+from modules.api_instance.domain import IRepository
+from modules.shared.domain import IQuery
+from modules.shared.domain import IResponse
+from modules.shared.domain import IUseCase
+
+
+class SearchApiQuery(IQuery):
+    def __init__(self, api_id: str) -> None:
+        self.api_id = api_id
+
+
+class SearchApiResponse(IResponse):
+
+    def __init__(self, _id: str, port: int, routes, settings):
+        self._id = _id
+        self.port = port
+        self.routes = routes
+        self.settings = settings
+
+
+class SearchApi(IUseCase):
+
+    def __init__(self, repository: IRepository) -> None:
+        self.repository = repository
+
+    def execute(self, command: SearchApiQuery):
+        api = self.repository.search(command.api_id)
+        return SearchApiResponse(**api.get_object_dict())
