75b6ae4df3dc8e1493e07bf9be60b19c2bd6a8b6
Andres Gutierrez P <andres.gutierrez@jalasoft.com>
2019-06-06 15:18:45 -0400

    Refactor name for collections of queries

--- src/modules/shared/infrastructure/QueryBus.py
+++ src/modules/shared/infrastructure/QueryBus.py
@@ -7,12 +7,12 @@ from modules.shared.domain.errors import QueryIsAlreadyRegistered
 class QueryBus(IBus):
 
     def __init__(self) -> None:
-        self.querys = dict()
+        self.queries = dict()
 
     def register(self, command: IQuery, use_case: IUseCase):
-        if command.__name__ in self.querys:
+        if command.__name__ in self.queries:
             raise QueryIsAlreadyRegistered(f'Command {command.__name__} is already registered')
-        self.querys[command.__name__] = use_case
+        self.queries[command.__name__] = use_case
 
     def execute(self, command: IQuery):
-        return self.querys[command.__class__.__name__].execute(command)
+        return self.queries[command.__class__.__name__].execute(command)
