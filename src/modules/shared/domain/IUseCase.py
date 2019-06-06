daf6a221edf46224585db501dd41fa3ee56c05b0
Andres Gutierrez P <andres.gutierrez@jalasoft.com>
2019-06-06 14:23:07 -0400

    Add query for CQRS architecture in domain

--- src/modules/shared/domain/IUseCase.py
+++ src/modules/shared/domain/IUseCase.py
@@ -1,6 +1,8 @@
 from modules.shared.domain import ICommand
+from modules.shared.domain import IQuery
+from modules.shared.domain import IResponse
 
 
 class IUseCase:
 
-    def execute(self, command: ICommand): pass
+    def execute(self, command: ICommand or IQuery) -> None or IResponse: pass
--- src/modules/shared/domain/__init__.py
+++ src/modules/shared/domain/__init__.py
@@ -4,3 +4,5 @@ from .IEntity import IEntity
 from .IPersistenceConnection import IPersistenceConnection
 from .IUseCase import IUseCase
 from .IBus import IBus
+from .IQuery import IQuery
+from .IResponse import IResponse
