4b0b8735f20a7a141c3e523c9c6e00bc220f12ce
Andres Gutierrez P <andres.gutierrez@jalasoft.com>
2019-06-06 16:05:15 -0400

    Refactor api controller

--- src/backend/controllers/ApiController.py
+++ src/backend/controllers/ApiController.py
@@ -1,12 +1,13 @@
 from flask import request
 from flask_restplus import Namespace, Resource
 
+from modules.api_instance import RegisterApiCommand
+from modules.api_instance import command_bus
+
 controller = Namespace('api', description='End points for created a api instance')
 
 
 @controller.route('')
 class ApiController(Resource):
     def post(self):
-        data_post = request.get_json()
-        print(data_post)
-        return 'success', 200
+        return command_bus.execute(RegisterApiCommand(**request.get_json())), 200
