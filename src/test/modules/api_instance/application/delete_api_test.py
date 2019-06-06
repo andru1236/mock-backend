fb7a2c10724544480c3de5ea7a590cf3aab4b2d5
Andres Gutierrez P <andres.gutierrez@jalasoft.com>
2019-06-06 13:58:28 -0400

    Implement delete api unit test

---
+++ src/test/modules/api_instance/application/delete_api_test.py
@@ -0,0 +1,23 @@
+from modules.api_instance import DeleteApi
+from modules.api_instance import DeleteApiCommand
+from modules.api_instance import RegisterApi
+from modules.api_instance import RegisterApiCommand
+from modules.api_instance.infrastructure import FakeRepository
+
+
+def test_delete_api():
+    repository = FakeRepository()
+    register_api = RegisterApi(repository)
+    register_api.execute(RegisterApiCommand(8000))
+
+    inserted_one_api_instance = 1
+    assert len(repository.apis) == inserted_one_api_instance
+
+    api_id = repository.apis[0]._id
+
+    use_case = DeleteApi(repository)
+    use_case.execute(DeleteApiCommand(api_id))
+
+    without_api = 0
+
+    assert len(repository.apis) == without_api
