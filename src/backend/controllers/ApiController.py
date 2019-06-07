47adb1069a6d7ecfc938ad8bc398b3773dfccb5e
Andres Gutierrez P <andres.gutierrez@jalasoft.com>
2019-06-07 13:51:10 -0400

    Change id name for api_id on api controller

--- src/backend/controllers/ApiController.py
+++ src/backend/controllers/ApiController.py
@@ -1,5 +1,3 @@
-import json
-
 from flask import request
 from flask_restplus import Namespace, Resource
 
@@ -19,24 +17,23 @@ class ApiController(Resource):
         return command_bus.execute(RegisterApiCommand(**request.get_json())), 201
 
 
-@controller.route('/<id>')
+@controller.route('/<api_id>')
 class ApiSearcherController(Resource):
-    def get(self, id):
-        return query_bus.execute(SearchApiQuery(id))
+    def get(self, api_id):
+        return query_bus.execute(SearchApiQuery(api_id))
 
 
-@controller.route('/<id>/routers')
+@controller.route('/<api_id>/routers')
 class RouteController(Resource):
-    def post(self, id):
+    def post(self, api_id):
         data = request.get_json()
-        print(data['response'])
-        return command_bus.execute(AddRouteCommand(id, data['path'], data['method'], data['response'])), 200
+        return command_bus.execute(AddRouteCommand(api_id, data['path'], data['method'], data['response'])), 200
 
 
-@controller.route('/<id>/start')
+@controller.route('/<api_id>/start')
 class StartController(Resource):
-    def post(self, id):
+    def post(self, api_id):
         repo = Repository()
-        api = repo.search(id)
+        api = repo.search(api_id)
         api.run_api()
         return 'success', 200
