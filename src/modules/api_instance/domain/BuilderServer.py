bcaec6ccf0d1b1acedea3a2cb953f6888e9f24b4
Andres Gutierrez P <andres.gutierrez@jalasoft.com>
2019-06-06 21:24:51 -0400

    Transform builder server on singleton

--- src/modules/api_instance/domain/BuilderServer.py
+++ src/modules/api_instance/domain/BuilderServer.py
@@ -5,6 +5,7 @@ from flask_restplus import Api
 from flask_restplus import Resource
 
 from modules.api_instance.domain import ApiInstance
+from modules.shared.infrastructure.utils import SingletonDecorator
 
 
 class MethodWithResponse:
@@ -28,14 +29,8 @@ class RouteUnifyByPath:
             self.resources.append(new_resource)
 
 
+@SingletonDecorator
 class BuilderServer:
-    # Singleton pattern
-    # __instance = None
-    #
-    # def __new__(cls, *args, **kwargs):
-    #     if cls.__instance is None:
-    #         cls.__instance = super(BuilderServer, cls)
-    #     return cls.__instance
 
     def __init__(self) -> None:
         self.threads = []
