b97fbf01d8a21559d75f2d2261944f1209ffcd21
Andres Gutierrez P <andres.gutierrez@jalasoft.com>
2019-06-10 16:09:27 -0400

    Add CORS to api

--- src/backend/Server.py
+++ src/backend/Server.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask
-
+from flask_cors import CORS
 from backend.apis import api_v1
 
 
@@ -9,6 +9,7 @@ class Server:
 
     def __init__(self) -> None:
         self.flask_server = Flask('Server')
+        CORS(self.flask_server)
         self.apis = [
             # TODO: Add new api versions
             api_v1
