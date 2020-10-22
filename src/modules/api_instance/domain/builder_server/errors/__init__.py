36cbb7eb18bdd8bbfdf081b6eff71c6e05744d3e
Ariel Ricaldez <ariel.ricaldez@jalasoft.com>
2020-10-22 12:05:45 -0400

    Create new NotEnoughResources error type

--- src/modules/api_instance/domain/builder_server/errors/__init__.py
+++ src/modules/api_instance/domain/builder_server/errors/__init__.py
@@ -1,3 +1,4 @@
 from .ServerIsRunning import ServerIsRunning
 from .ServerNeverWasStarting import ServerNeverWasStarting
 from .PortIsBusy import PortIsBusy
+from .NotEnoughResources import NotEnoughResources
